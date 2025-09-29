4272003856ed0dc659377b51e7d0fc79bc82838c
Julian Brünn <julian.bruenn@alberts.de>
2025-09-29 06:54:25 +0200

    refactor(constants): comment out unused guide constants for future consideration

--- src/constants.py
+++ src/constants.py
@@ -9,6 +9,12 @@ FLAG_TO_TAG: Final[dict[str, str]] = {
     "GUIDE_ARCHIVED": "archived",
     "GUIDE_STARRED": "starred",
     "GUIDE_USER_CONTRIBUTED": "user_contributed",
+    # I will think of a better way to do this in the future.
+    # "GUIDE_IN_PROGRESS": "in_progress",
+    # "GUIDE_MISSING_IMAGES": "missing_images",
+    # "GUIDE_MISSING_STEPS": "missing_steps",
+    # "INTRODUCTION_ISSUES": "introduction_issues",
+    # "NO_DETAILS": "no_details"
 }
 
 METADATA_KEYS: Final[set[str]] = {
